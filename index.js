e225c2ec6c4a810b55078f505e77050eb6f0875f
Gergő Móricz <mo.geryy@gmail.com>
2017-11-18 11:27:27 +0100

    Main script

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+var logger = require('./lib/logger');
+var commands = require('./lib/commands');
+
+const token = process.env.BOAT_TOKEN;
+
+var client = new Discord.Client();
+
+client.on('ready', function() {
+    logger("bot", "Ready!");
+    client.user.setGame("Sailing | +hello")
+});
+
+client.on('message', function(message) {
+    var command = commands.find(function(element){
+        if (element.regex.exec(message.content)) return true; else return false;
+    });
+    if (command) {
+        command.method(message, command.regex.exec(message.content), commands); //1: message, 2: args, 3: other commands
+    }
+});
+
+client.login(token);
